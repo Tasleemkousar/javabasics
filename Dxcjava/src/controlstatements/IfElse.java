6b22cab305c76925733385e967f6d3e2868ebc43
lenovo <lenovo@DESKTOP-7ANPUP3>
2021-10-19 13:56:55 +0530

    control statements

---
+++ Dxcjava/src/controlstatements/IfElse.java
@@ -0,0 +1,11 @@
+package controlstatements;
+
+public class IfElse {
+	public static void main(String[] args) {
+		int number = 26;
+		String output = (number % 2 == 0) ? "even number" : "odd number" ;
+		System.out.println(output);
+		
+	}
+
+}
---
+++ Dxcjava/src/controlstatements/ifExample.java
@@ -0,0 +1,11 @@
+package controlstatements;
+
+public class ifExample {
+ public static void main(String[] args) {
+	 int age = 20;
+	 if(age>18) {
+		 System.out.println("age is greater than 18");
+	 }
+ }
+
+}
