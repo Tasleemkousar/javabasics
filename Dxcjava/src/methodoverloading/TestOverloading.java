084c4d38546203efa4b2dc2e8adf7ba28583392a
lenovo <lenovo@DESKTOP-7ANPUP3>
2021-10-20 16:35:07 +0530

    methodoverloading

---
+++ Dxcjava/src/methodoverloading/TestOverloading.java
@@ -0,0 +1,11 @@
+package methodoverloading;
+
+public class TestOverloading {
+
+	public static void main(String[] args) {
+
+		System.out.println(Adder.add(11,1));
+		System.out.println(Adder.add(12.3, 12.6));
+	}
+
+}
