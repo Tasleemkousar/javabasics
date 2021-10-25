dba155251207638702aebd83cc36e87805c9c4a4
lenovo <lenovo@DESKTOP-7ANPUP3>
2021-10-25 09:40:21 +0530

    sting

---
+++ Dxcjava/src/stringpractise/SubString.java
@@ -0,0 +1,15 @@
+package stringpractise;
+
+import java.util.Scanner;
+
+public class SubString {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String a=scan.nextLine();
+		int b=scan.nextInt();
+		int c=scan.nextInt();
+		System.out.println(a.substring(b,c));
+
+	}
+
+}
